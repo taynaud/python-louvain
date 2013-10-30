480a92986a24e91fea189ae06cd246e8664a08b1
Luke Cyca <me@lukecyca.com>
2013-10-30 16:36:27 -0700

    Created an installable package suitable for PyPI

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name="python-louvain",
+    version="0.1",
+    author="Thomas Aynaud",
+    author_email="thomas.aynaud@lip6.fr",
+    description="Louvain algorithm for community detection",
+    license="BSD",
+    url="http://perso.crans.org/aynaud/communities/",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+    ],
+
+    packages=['community'],
+    install_requires=[
+        "networkx",
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'community = community:main',
+        ]
+    }
+)
