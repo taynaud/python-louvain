a4b9fe6603143222f57c1e492e8da21762495696
Thomas Aynaud <t.aynaud@qwant.com>
2018-12-20 16:01:50 +0100

    Raise version to 0.12

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="python-louvain",
-    version="0.11",
+    version="0.12",
     author="Thomas Aynaud",
     author_email="taynaud@gmail.com",
     description="Louvain algorithm for community detection",
